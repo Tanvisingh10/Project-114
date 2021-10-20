815ccac24f0c632142430d484633e8bccf434c36
Tanvisingh10 <80450565+Tanvisingh10@users.noreply.github.com>
2021-10-20 20:25:47 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(400,350);
+    canvas.position(850,200);
+}
+function draw(){
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
